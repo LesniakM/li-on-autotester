c4989caf06981b18ece52401cfb92d6b0b636fa9
Mariusz Lesniak <lesniak.mar@gmail.com>
2023-04-06 21:41:02 +0200

    Added more functions used to draw on display.

--- include/display.h
+++ include/display.h
@@ -9,11 +9,15 @@ class OledDisplay
 public:
   OledDisplay();
   void drawStaticDisplayElements();
+  void drawSplashScreen();
+  void clear();
   void updateDisplayCapacity(uint16_t capacity);
   void updateDisplayTemperature(float temperature);
   void updateDisplayVoltage(uint16_t voltage);
   void updateDisplayCurrent(int16_t current);
   void updateDisplayState(uint8_t state);
+  void updateClock(uint8_t minutes, uint8_t seconds);
+  void clearClock();
 };
 
 #endif /* DISPLAY_H */
--- src/display.cpp
+++ src/display.cpp
@@ -33,8 +33,9 @@
 */
 
 #include <display.h>
+#include <string>
 
-constexpr uint8_t INFO_ROW = 0;
+constexpr uint8_t INFO_ROW = 3;
 constexpr uint8_t INFO_COL = 0;
 constexpr uint8_t CAPACITY_ROW = 0;
 constexpr uint8_t VOLTAGE_ROW = 1;
@@ -46,16 +47,32 @@ U8X8_SSD1306_128X32_UNIVISION_HW_I2C u8x8(/* reset=*/ U8X8_PIN_NONE, /* clock=*/
 OledDisplay::OledDisplay() {
   u8x8.begin();
   u8x8.clear();
+  drawSplashScreen();
   drawStaticDisplayElements();
+}
+
+void OledDisplay::drawSplashScreen(){
+  u8x8.setFont(u8x8_font_8x13B_1x2_r);
+  u8x8.drawString(0, 0, "     Dotigo     ");
+  delay(750);
+  u8x8.drawString(0, 2, "P r e s e n t s ");
+  delay(750);
+  u8x8.drawString(0, 0, "     LI-ION     ");
+  u8x8.drawString(0, 2, "   AUTOTESTER   ");
+  delay(750);
+  clear();
+}
+
+void OledDisplay::clear(){
+  u8x8.clear();
 }
 
 void OledDisplay::drawStaticDisplayElements() {
   u8x8.setFont(u8x8_font_amstrad_cpc_extended_f);
-  u8x8.drawString(0, CAPACITY_ROW,"     IR:    mOhm");
+  u8x8.drawString(0, CAPACITY_ROW,"Capacit:     mAh");
   u8x8.drawString(0, VOLTAGE_ROW, "Voltage:      mV");
   u8x8.drawString(0, CURRENT_ROW, "Current:      mA");
-  u8x8.drawString(0, TEMP_ROW,    "   Temp:");
-  u8x8.drawString(0, TEMP_ROW,    "   ");
+  u8x8.drawString(0, TEMP_ROW,    "            T:  ");
 }
 
 void OledDisplay::updateDisplayCapacity(uint16_t capacity) {
@@ -83,13 +100,14 @@ void OledDisplay::updateDisplayCurrent(int16_t current) {
 }
 
 void OledDisplay::updateDisplayTemperature(float temperature) {
-  char stemp[6];
+  char stemp[3]; // 6 for decimal point
   u8x8.setFont(u8x8_font_artossans8_r);
-  dtostrf(temperature, 3, 1, stemp);
-  u8x8.drawString(8, TEMP_ROW, stemp);
-  u8x8.setFont(u8x8_font_amstrad_cpc_extended_f);
-  u8x8.drawGlyph(14, TEMP_ROW, 176);
-  u8x8.drawString(15, TEMP_ROW, "C");
+  itoa(int(temperature), stemp, 10);
+  //dtostrf(temperature, 3, 1, stemp);
+  u8x8.drawString(14, TEMP_ROW, stemp);
+  // u8x8.setFont(u8x8_font_amstrad_cpc_extended_f);
+  // u8x8.drawGlyph(14, TEMP_ROW, 176);
+  // u8x8.drawString(15, TEMP_ROW, "C");
 }
 
 
@@ -97,11 +115,11 @@ void OledDisplay::updateDisplayTemperature(float temperature) {
  * Prints state code on display.
  *
  * 0 - OK
- * 1 - CHG
- * 2 - DCH
- * 3 - IDL
- * 4 - IR
- * 5 - II
+ * 1 - CHG CC
+ * 2 - CHG CV
+ * 3 - Wait
+ * 4 - DSC CC
+ * 5 - IR TST
  * 6 - HOP
  * 7 - HCL
  * 99 - 
@@ -112,31 +130,31 @@ void OledDisplay::updateDisplayState(uint8_t state) {
   switch (state)
   {
   case 0:
-    u8x8.drawString(INFO_COL, INFO_ROW, "OK ");
+    u8x8.drawString(INFO_COL, INFO_ROW, "  OK  ");
     break;
   case 1:
-    u8x8.drawString(INFO_COL, INFO_ROW, "CHG");
+    u8x8.drawString(INFO_COL, INFO_ROW, "CHG CC");
     break;
   case 2:
-    u8x8.drawString(INFO_COL, INFO_ROW, "DCH");
+    u8x8.drawString(INFO_COL, INFO_ROW, "CHG CV");
     break;
   case 3:
-    u8x8.drawString(INFO_COL, INFO_ROW, "IDL");
+    u8x8.drawString(INFO_COL, INFO_ROW, "Wait: ");
     break;
   case 4:
-    u8x8.drawString(INFO_COL, INFO_ROW, "IR ");
+    u8x8.drawString(INFO_COL, INFO_ROW, "DSC CC");
     break;
   case 5:
-    u8x8.drawString(INFO_COL, INFO_ROW, "II ");
+    u8x8.drawString(INFO_COL, INFO_ROW, "IR TST");
     break;
   case 6:
-    u8x8.drawString(INFO_COL, INFO_ROW, "HOP");
+    u8x8.drawString(INFO_COL, INFO_ROW, " HOP  ");
     break;
   case 7:
-    u8x8.drawString(INFO_COL, INFO_ROW, "HCL");
+    u8x8.drawString(INFO_COL, INFO_ROW, " HCL  ");
     break;
   case 99:
-    u8x8.drawString(INFO_COL, INFO_ROW, "   ");
+    u8x8.drawString(INFO_COL, INFO_ROW, "      ");
     break;
   case 101:
     u8x8.drawString(INFO_COL, INFO_ROW, " I ");
@@ -162,8 +180,41 @@ void OledDisplay::updateDisplayState(uint8_t state) {
   case 100:
     u8x8.drawString(INFO_COL, INFO_ROW, " X ");
     break;
+  case 201:
+    u8x8.drawString(INFO_COL, INFO_ROW, "Cr low");
+    break;
+  case 202:
+    u8x8.drawString(INFO_COL, INFO_ROW, "V err ");
+    break;
   default:
-    u8x8.drawString(INFO_COL, INFO_ROW, "ERR");
+    u8x8.drawString(INFO_COL, INFO_ROW, " ERR  ");
     break;
   }
+}
+
+/**
+ * Prints time in info_row.
+ *
+ * @param minutes
+ * @param seconds
+ */
+void OledDisplay::updateClock(uint8_t minutes, uint8_t seconds) {
+  u8x8.setFont(u8x8_font_artossans8_r);
+  std::string text = "";
+  text += std::to_string(minutes);
+  if (text.length() < 2) text.insert(0, "0");
+  text += ":";
+  text += std::to_string(seconds);
+  if (text.length() < 5) text.insert(text.size()-1, "0");
+  u8x8.setFont(u8x8_font_artossans8_r);
+  u8x8.drawString(5, INFO_ROW, text.c_str());
+}
+
+/**
+ * Clears area where time is normally diplayed.
+ *
+ */
+void OledDisplay::clearClock() {
+  u8x8.setFont(u8x8_font_artossans8_r);
+  u8x8.drawString(5, INFO_ROW, "     ");
 }
